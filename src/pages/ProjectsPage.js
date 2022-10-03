99e06b495ee3c8d37dabd7d7d1f88f3fac4d80ea
Sofie <sofie.kodehode@gmail.com>
2022-10-03 11:02:49 +0200

    added todolist to ProjectsPage

--- src/pages/ProjectsPage.js
+++ src/pages/ProjectsPage.js
@@ -1,4 +1,15 @@
 import ProjectShop from "./projectsPage/img/ProjectShop.png";
+import todolist from "./projectsPage/img/todolist.png";
+import {
+  Column,
+  Container,
+  Image,
+  ImagePhone,
+  MarginDiv,
+  NewSection,
+  StyledButton,
+  Tittel,
+} from "../styles/styles";
 
 import {
   BoxDiv,
@@ -11,17 +22,38 @@ const Projects = () => {
     <MainDiv>
       <h1>Projekter</h1>
       <br></br>
-      <BoxDiv>
-        <h2>Nettbutikk</h2>
-        <a
-          href="https://masoer.github.io/summertask/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <WebshopImage src={ProjectShop} alt="webshop" type="button" />
-        </a>
-        <p>Laget i React</p>
-      </BoxDiv>
+      <Container>
+        <NewSection>
+          <Column>
+            <BoxDiv>
+              <h2>Nettbutikk</h2>
+              <a
+                href="https://masoer.github.io/summertask/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <WebshopImage src={ProjectShop} alt="webshop" type="button" />
+              </a>
+              <p>Laget i React</p>
+            </BoxDiv>
+            <Container>
+              <MarginDiv>
+                <BoxDiv>
+                  <h2>To do list</h2>
+                  <a
+                    href="https://github.com/Masoer/todolist"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <WebshopImage src={todolist} alt="webshop" type="button" />
+                  </a>
+                  <p>Laget med javascript</p>
+                </BoxDiv>
+              </MarginDiv>
+            </Container>
+          </Column>
+        </NewSection>
+      </Container>
     </MainDiv>
   );
 };
